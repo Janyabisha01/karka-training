8cfc0718ae8a5c73c989ce9202fcbde606b148fe
Janyabisha01 <abisha2018j@gmail.com>
2022-10-10 17:08:10 +0530

    javascript project

---
+++ 10_10_22/comorins/Output/main.js
@@ -0,0 +1,34 @@
+let news={};
+function add(key,value,index=null){
+    if(key=="news_type" || key=="category")
+    {
+        if(!news[key])
+        {
+            news[key]=[];
+        }
+        news[key][index]=value;
+    }else{
+        news[key]=value;
+    }
+    console.log(news)
+}
+function display(){
+    document.getElementById("datas").innerHTML=JSON.stringify(news);
+    if(!localStorage.getItem('news'))
+    {
+        localStorage.setItem('news',JSON.stringify([]))
+    }
+    let newscontent=JSON.parse(localStorage.getItem('news'));
+    newscontent.push(news);
+    localStorage.setItem('news',JSON.stringify(newscontent));
+    newsdisplay()
+}
+function newsdisplay(){
+    let element=""
+    let newsindex=JSON.parse(localStorage.getItem("news"))
+    for(let i=0;i<newsindex.length;i++){
+        element=element+"<li> <a href='view.html?id="+newsindex[i].id +"'target='_blank'>"+newsindex[i].id+"</a></li>"
+    }
+    document.getElementById("newslist").innerHTML=element
+}
+newsdisplay()
